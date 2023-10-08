0851f4b027f17cdf37d2112e94e230dba38e5146
Tim Dove <timdoveemail@yahoo.com>
2023-10-07 22:31:35 -0400

    Stabilize after adding Drizzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+//require("dotenv").config();
+
+// import * as dotenv from "dotenv";
+// dotenv.config();
+//dotenv.config({ path: __dirname + "/.env" });
+// import "dotenv/config";
+
+// Connection to local mysql
+// import { drizzle } from "drizzle-orm/mysql2";
+// import mysql from "mysql2/promise";
+
+// console.log(process.env);
+
+// const connection = await mysql.createConnection({
+//   host: "host",
+//   user: "user",
+//   database: "database",
+// });
+
+// DB_LOCAL_CONNECTION=mysql
+// DB_LOCAL_HOST=127.0.0.1
+// DB_LOCAL_PORT=3306
+// DB_LOCAL_DATABASE=job_tracker
+// DB_LOCAL_USERNAME=root
+// DB_LOCAL_PASSWORD=whiteTable1
+
+//const db = drizzle(connection);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +56,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
