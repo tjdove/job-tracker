89e89767872d7391966c733547d86ad7822433c1
Tim Dove <timdoveemail@yahoo.com>
2023-10-11 21:09:04 -0400

    Install/Config DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1></h1>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="w-96 bg-white shadow rounded">TheTest</div>
+      </div>
+      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 40)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
     </>
   );
